d81a2d688c42b9a3d335b74e8e7e2eb0e3683bfd
Kabildev C <kabildev@proglint.com>
2023-07-26 14:13:09 +0530

    Comments updated for the batch size and db connection creation

--- script.py
+++ script.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from sqlalchemy import create_engine
- 
+
+# create an database connection with sqlalchemy
 engine = create_engine('postgresql+psycopg2://postgres:Qm7pn8mJZ007@localhost/assesment')
 
 def process_data(path):
     # Specify the batch size to read from CSV file and INSERT in SQL
+    # This batch size depends on the SQL server performance
     chunksize = 10 ** 3
     with pd.read_csv(path, chunksize=chunksize) as reader:
         for chunk in reader:
